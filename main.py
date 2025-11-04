96fb42bbbb7b30dbc4285c5d386827b14c93f93a
fred-git121 <frederic.delville@hotmail.fr>
2025-11-04 14:10:30 +0100

    Added feature to loop through all the property characteristics of a given URL

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import requests
+import requests, pandas as pd, re, random
 from bs4 import BeautifulSoup
-import random
-import pandas
 
 user_agents = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
@@ -10,34 +8,42 @@ user_agents = [
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15'
 ]
-
-url = "https://immovlan.be/fr/immobilier?transactiontypes=a-vendre,en-vente-publique&noindex=1"
 headers = {'User-Agent': random.choice(user_agents)}
-response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.text, 'html.parser')
 
-html = requests.get(url, headers=headers).text
-soup = BeautifulSoup(html, 'html.parser')
+url = "https://immovlan.be/en/detail/investment-property/for-sale/7060/soignies/rbu62401"
+soup = BeautifulSoup(requests.get(url, headers=headers).text, "html.parser")
 
-listings = []
+specs = {}
 
-for card in soup.find_all("article", class_="list-view-item"):
-    id = card.find().get_text(strip=True)
-    title = card.select_one("h2.card-title a").get_text(strip=True)
-    price = card.select_one("strong.list-item-price").get_text(strip=True)
-    locality= card.select_one("span[itemprop='addressLocality']").get_text(strip=True)
-    link = card.select_one("h2.card-title a")["href"]
-    vlncode = link.rstrip("/").split("/")[-1]
+# <ul><li><strong>Label:</strong> Value</li>
+for li in soup.select("ul li"):
+    txt = li.get_text(strip=True)
+    m = re.match(r"^(.+?):\s*(.+)$", txt)  # "Label: Value"
+    if m:
+        specs[m.group(1)] = m.group(2)
 
-    listings.append({
-        'id': vlncode,
-        "title": title,
-        "price": price,
-        "Locality": locality
-    })
+# Every <h4><p> pair on the page
+for h4 in soup.select("h4"):
+    p = h4.find_next("p")
+    if p:
+        label = h4.get_text(strip=True)
+        val = p.get_text(strip=True)
+        specs[label] = val
 
-# Print results
-for item in listings:
-    print(item)
-# first_card = soup.find("article", class_="list-view-item")
-# print(first_card.prettify())
+# ---- Description ----
+desc_tag = soup.select_one("div:has(> h2:-soup-contains('Description')) + div")
+if desc_tag:
+    specs["Description"] = desc_tag.get_text(" ", strip=True)
+
+# ---- View ----
+df = pd.DataFrame([specs])
+print(df.T)    # transpose for quick read
+df.to_csv("immovlan_one_page.csv", index=False)
+
+# url = "https://immovlan.be/en/detail/investment-property/for-sale/7060/soignies/rbu62401"
+# soup = BeautifulSoup(requests.get(url, headers=headers).text, "html.parser")
+#
+# # 1. Every H3 text
+# print("H3 texts:")
+# for h in soup.select("h3"):
+#     print("-", h.get_text(strip=True))
